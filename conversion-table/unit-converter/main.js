a8fa34ad51fdd7edb63ed398e7d9523c9684913d
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-08-28 23:13:03 -0700

    conversion table completely functional

--- conversion-table/unit-converter/main.js
+++ conversion-table/unit-converter/main.js
@@ -1,54 +1,89 @@
 $(document).ready(initializeApp)
 
-function initializeApp(){
-    $('.convert-button').click(convert)
+
+
+function initializeApp() {
+    $('.convert-button').click(convert);
+    $('.grams').css('display', 'none')
 }
 
-function calculateAnswer(value, units){
+function removeDuplicate(){
+    if($('.unit-drop-down option:selected').val() === 'grams'){
+        $('.grams').css('display', 'none')
+        $('.kilograms').css('display', 'block')
+    }
+    else{
+        $('.kilograms').css('display', 'none')
+        $('.grams').css('display', 'block')
+    }
+}
+
+function calculateAnswer(value, units) {
     var answer = value * .865;
     answer = answer.toFixed(4)
     appendAnswer(answer, units)
 }
 
-function appendAnswer(answer, units){
+function appendAnswer(answer, units) {
     $('.answer-number').text(answer);
     $('.answer-units').text(units)
 }
 
-function pintConversion(inputValue, units){
-    console.log(inputValue);
-}
 
-function convert(){
+
+function convert() {
     var inputValue = parseFloat($('input').val())
-    switch($('.unit-drop-down option:selected').val()) {
+    switch ($('.unit-drop-down option:selected').val()) {
         case 'grams':
-            switch($('.to-unit-drop-down option:selected').val()){
+            switch ($('.to-unit-drop-down option:selected').val()) {
                 case 'mL':
-                calculateAnswer(inputValue, 'mL');
-                break;
+                    calculateAnswer(inputValue, 'mL');
+                    break;
                 case 'L':
-                convertedValue = inputValue/1000;
-                calculateAnswer(convertedValue, 'L')
-                break;
+                    convertedValue = inputValue / 1000;
+                    calculateAnswer(convertedValue, 'L')
+                    break;
                 case 'oz':
-                var answer = inputValue * 0.035274
-                appendAnswer(answer, 'oz')
-                break;
+                    var answer = inputValue * 0.035274
+                    appendAnswer(answer, 'oz')
+                    break;
                 case 'kg':
-                var answer = inputValue / 1000;
-                appendAnswer(answer, 'kg')
-                break;
+                    var answer = inputValue / 1000;
+                    appendAnswer(answer, 'kg')
+                    break;
                 case 'pint':
-                pintConversion(inputValue, 'pints')
-                var mLConversion = inputValue * .865;
-                var pintAnswer = mLConversion * 0.00211338;
-                appendAnswer(pintAnswer, 'pints')
-                break;
+                    var mLConversion = inputValue * .865;
+                    var pintAnswer = mLConversion * 0.00211338;
+                    pintAnswer = pintAnswer.toFixed(4);
+                    appendAnswer(pintAnswer, 'pints')
+                    break;
             }
             break;
         case 'kg':
-            console.log('kg')
+            switch ($('.to-unit-drop-down option:selected').val()) {
+                case 'mL':
+                    inputValue = inputValue * 1000
+                    calculateAnswer(inputValue, 'mL');
+                    break;
+                case 'L':
+                    calculateAnswer(inputValue, 'L')
+                    break;
+                case 'oz':
+                    var answer = inputValue * 35.274
+                    appendAnswer(answer, 'oz')
+                    break;
+                case 'grams':
+                    var answer = inputValue * 1000;
+                    appendAnswer(answer, 'grams')
+                    break;
+                case 'pint':
+                    inputValue = inputValue * 1000
+                    var mLConversion = inputValue * .865;
+                    var pintAnswer = mLConversion * 0.00211338;
+                    pintAnswer = pintAnswer.toFixed(4);
+                    appendAnswer(pintAnswer, 'pints')
+                    break;
+            }
             break;
         default:
             console.log('error')
